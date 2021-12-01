f0b80b3b40f8d2c6220d9f2840fcaf6c18dd6b18
Ельвіра Матраєва <e.matrayeva@kpnu.km.ua>
2021-12-01 14:36:29 +0300

    Добавьте файлы проекта.

---
+++ MKR_oop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MKR_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square s1 = new Square();
+            s1.side = 5;
+            s1.type = "quadrate";
+            s1.DrawType();
+            s1.DrawSquare();
+        }
+    }
+    public interface IShape
+    {
+        void DrawType();
+        void DrawSquare();
+    }
+    public class Square : IShape
+    {
+        public string type;
+        public int side;
+        public void DrawType()
+        {
+            Console.WriteLine("This is: " + type);
+        }
+        public void DrawSquare()
+        {
+            Console.WriteLine("Square equal: " + side*side);
+        }
+    }
+}
